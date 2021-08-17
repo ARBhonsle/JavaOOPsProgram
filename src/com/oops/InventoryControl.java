43e63bde0f3d98341f32123f405bcc2dd481680c
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-17 17:55:43 +0530

    Added: Project files

---
+++ src/com/oops/InventoryControl.java
@@ -0,0 +1,8 @@
+package com.oops;
+
+public class InventoryControl {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to JSON Inventory Data Management");
+    }
+}
