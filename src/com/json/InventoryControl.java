84bb40002995e36f004b6767fa592f4f374ba5b6
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-24 21:20:54 +0530

    Added: Inventory Management

--- src/com/json/InventoryControl.java
+++ src/com/json/InventoryControl.java
@@ -7,12 +7,26 @@ import java.io.FileReader;
 import java.util.*;
 
 public class InventoryControl {
+    //variables
+    static Double weight, price, amount;
+
     // Prints to detail of given object typecast as MAP
-    public static void printDetails(Map dataDetails){
+    public static void printDetails(Map dataDetails) {
+        weight=null; price=null;
         Iterator<Map.Entry> itr1 = dataDetails.entrySet().iterator();
         while (itr1.hasNext()) {
             Map.Entry pair = itr1.next();
             System.out.println(pair.getKey() + " : " + pair.getValue());
+            if (pair.getKey().equals("price")) {
+                price = Double.parseDouble(pair.getValue().toString());
+            }
+            if (pair.getKey().equals("weight")) {
+                weight = Double.parseDouble(pair.getValue().toString());
+            }
+            if (price != null && weight != null) {
+                amount = weight * price;
+                System.out.println("Total Amount : " + amount);
+            }
         }
     }
 
@@ -23,17 +37,17 @@ public class InventoryControl {
             // reads path to json file
             Object details = parser.parse(new FileReader("src/resources/inventoryDetails.json"));
             // typecasting JSONObject
-            JSONObject obj=(JSONObject) details;
+            JSONObject obj = (JSONObject) details;
             // prints JSONObject
             // printDetails(obj);
             // typecast to Map prints String to console
-            Map dataDetails = ((Map)obj.get("Rice"));
+            Map dataDetails = ((Map) obj.get("Rice"));
             System.out.println("Rice--------");
             printDetails(dataDetails);
-            dataDetails = ((Map)obj.get("Wheat"));
+            dataDetails = ((Map) obj.get("Wheat"));
             System.out.println("Wheat-------");
             printDetails(dataDetails);
-            dataDetails = ((Map)obj.get("Pulses"));
+            dataDetails = ((Map) obj.get("Pulses"));
             System.out.println("Pulses------");
             printDetails(dataDetails);
         } catch (Exception e) {
